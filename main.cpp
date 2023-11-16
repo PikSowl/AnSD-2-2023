a8c92b5cc8586557a9cd477f0d8b153209bc72e7
PikSowl <kmazalov11@gmai.com>
2023-11-17 00:43:05 +0200

    stack of my function is not my stack

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
 ﻿#define RusLoc _setmode(_fileno(stdout), _O_U16TEXT); _setmode(_fileno(stdin),  _O_U16TEXT); _setmode(_fileno(stderr), _O_U16TEXT)
-int num[] = {0,1,2,3,4,5,6,7,8,9};
-int brack[] = {'[','(','{',']',')','}'};
-int oper[] = {'+','-','*','/','^'};
 
 #include <iostream>
 #include <fcntl.h>
 #include <algorithm>
 #include <string>
 #include <cmath>
+#include <stack>
 
 using std::wcout;
 using std::wcin;
@@ -16,86 +14,112 @@ using std::endl;
 using std::wstring;
 using std::find;
 using std::sqrt;
+using std::stack;
 
 double numMaker(wstring& eq, int& i){
     int transfer = int(eq[i])-48;
     int dotPosition = -1;
-    while ((int(eq[i+1])-48 < 10 && int(eq[i+1])-48 >=0)||eq[i+1] == '.'){
-        if (eq[i+1] == '.') dotPosition = i+1;
-        else transfer = transfer*10 + int(eq[i+1])-48;
+    for (;int(eq[i+1])-48 < 10 && int(eq[i+1])-48 >=0;i++) transfer = transfer*10 + int(eq[i+1])-48;
+    if (eq[i+1] == '.') {
+        dotPosition = i+1;
         i++;
     }
+    for (;int(eq[i+1])-48 < 10 && int(eq[i+1])-48 >=0;i++) transfer = transfer*10 + int(eq[i+1])-48;
     if (dotPosition == -1) dotPosition = i;
     return transfer / (pow(10,i - dotPosition));
 }
 
-double mathematics (double a, double b,char operTipe){
-    switch (operTipe) {
-        case '+': return a + b;
-        case '-': return a - b;
-        case '*': return a * b;
+double mathematics (double &a, double &b,wchar_t operation){
+    switch (operation) {
+        case '+':{
+            return a += b;
+        }
+        case '-':{
+            return a -= b;
+        }
+        case '*':{
+            return a *= b;
+        }
         case '/': {
             if (b == 0) {
                 wcout << L"деление на 0" << endl;
-                return -9999999;
+                return - 999999;
             }
-            else return a / b;
+            else return a /= b;
         }
-        case '^': return pow(a,b);
-        default: return -9999999;
+        default:
+            return - 999999;
     }
+}
+
+double mathReader (wstring &eq, int &i){
+    double temp;
+    stack<double> numbers;
+    stack<wchar_t> operations;
+    int len = eq.length();
+    int bt;
+
+    for (; i < len; i++){
+        if (eq[i] == '[' || eq[i] == '(' || eq[i] == '{'){
+            bt =i;
+            i++;
+            numbers.push(mathReader(eq,i));
+            if (numbers.top() == -999999) return - 999999;
+            if (!((eq[bt] == '[' && eq[i] == ']')||(eq[bt] == '(' && eq[i] == ')')||(eq[bt] == '{' && eq[i] == '}'))){
+                wcout << L"Ошибка в записи скобок" << endl;
+                return - 999999;
+            }
+        }
+        else if (eq[i] == '=' && i + 1 != len){
+            wcout << L"Равенство не в конце выражения" << endl;
+            return - 999999;
+        }
+
+        else if (int(eq[i])-48 < 10 && int(eq[i])-48 >=0) numbers.push(numMaker (eq, i));
+
+        else if (eq[i] == '+' || eq[i] == '-' || eq[i] == '*' || eq[i] == '/') {
+            operations.push(eq[i]);
+            if (operations.size() > numbers.size()) {
+                wcout << L"Ошибка в записи действий" << endl;
+                return - 999999;
+            }
+            if(!operations.empty() && operations.size() < numbers.size())
+                if (operations.top() == '*' || operations.top() == '/') {
+                    temp = numbers.top();
+                    numbers.pop();
+                    numbers.top() = mathematics(numbers.top(), temp, operations.top());
+                    operations.pop();
+                }
+        }
+
+        else if (eq[i] == ']' || eq[i] == ')' || eq[i] == '}'){
+            i--;
+            len = i;
+        }
+    }
+    while (!operations.empty()){
+        temp = numbers.top();
+        numbers.pop();
+        numbers.top() = mathematics(numbers.top(), temp, operations.top());
+        operations.pop();
+    }
+
+    return numbers.top();
 }
 
 int main() {
     RusLoc;
-    int len = 10;
-    wstring brackets;
-    char operTipe = 'o';
-    double a = -1, b;
     wstring eq;
+    int i = 0;
 
     wcout << L"Введите математическое выражение" << endl;
     wcin >> eq;
-    len = int(eq.length());
 
-    if (eq[len - 1]!='='){
-        wcout << L"Математическое выражение должно оканчиваться =";
+    if (eq[eq.length() - 1]!='='){
+        wcout << L" Математическое выражение должно оканчиваться =";
         return 0;
     }
 
-    for(int i =0;i < len - 1; i++){
-        if (find(brack,brack+3,eq[i]) != brack+3){
-            brackets.push_back(eq[i]);
-        }
-
-        else if (find(brack+3,brack+6,eq[i]) != brack+6){
-            if (!((brackets.back() == brack[0] && eq[i] == brack[3]) || (brackets.back() == brack[1] && eq[i] == brack[4]) || (brackets.back() == brack[2] && eq[i] == brack[5]))){
-                wcout << L"Некоректный порядок скобок";
-                return 0;
-            }
-            brackets.pop_back();
-        }
-
-        else if (find(num,num+10,int(eq[i]) - 48) != num+10){
-            b = a;
-            a = numMaker(eq,i);
-            if (b != -1) {
-                a = mathematics(b, a, operTipe);
-                if (a == -9999999) return -1;
-                operTipe = 'o';
-            }
-        }
-
-        else if (find(oper,oper+5,eq[i]) != oper+5){
-            if (operTipe != 'o') {
-                wcout << L"Нет числа между действиями";
-                return 0;
-            }
-            operTipe = char(eq[i]);
-        }
-    }
-
-    wcout << a;
-
+    wcout << mathReader(eq,i);
     return 0;
 }
